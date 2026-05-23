1c1b926d0c583e919b2c5c3b14a1d983e7df8e7b
sahilchaudhari2002 <sahilchaudhari2002@gmail.com>
2026-05-23 15:31:10 +0530

    Added database migrations

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
- 
+
 #!/usr/bin/env bash
 
 set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py makemigrations
 
 python manage.py migrate
+
+python manage.py collectstatic --no-input
  
